79f09b3851bc846a83273925cc27259ff6935e53
HuangChenrui <475273498@qq.com>
2020-12-25 11:43:38 +0800

    change c51 maybe have bugs

--- micro_controller_c51/uart.h
+++ micro_controller_c51/uart.h
@@ -36,32 +36,35 @@ void UART() interrupt 4 {
   if(RI == 1) {
 	int i = 0;//计数器
 	num = SBUF;  //取出数据
-	while (num != '!') {
-	  Received_Buf[i] = num;
+	if (num == '[') {
+	  while (num != ']') {
+	    Received_Buf[i] = num;
+	    RI = 0;
+	    delay(1);
+	    num = SBUF;
+	    i++;    
+	  }
+	  Received_Buf[i] = ']';
+	  Received_Buf[i+1] = '\0'; 
 	  RI = 0;
-	  delay(1);
-	  num = SBUF;
-	  i++;    
+	  //JUDGE FANS STAGE
+	  if (Received_Buf[5] == '0') FAN_FLAG = 0;
+	  else if (Received_Buf[5] == '1') FAN_FLAG = 1;
+	  else if (Received_Buf[5] == '2') FAN_FLAG = 2;
+	  else if (Received_Buf[5] == '3') FAN_FLAG = 3;
+	  else FAN_FLAG = 0;
+	  //JUDGE TIME
+	  if (Received_Buf[1] >= '6') TIME_FLAG = 0;
+	  else TIME_FLAG = 1;
+	  if (TIME_FLAG) {
+	    TIME_MIN = (Received_Buf[1] - 0x30) * 10 + (Received_Buf[2]-0x30);
+	    TIME_SEC = (Received_Buf[3] - 0x30) * 10 + (Received_Buf[4]-0x30);
+	  } 
+	  //UART_send_string(Received_Buf);
+	  for (i=19;i>0;i--)
+	    Received_Buf[i] = 0;
 	}
-	Received_Buf[i] = '!';
-	Received_Buf[i+1] = '\0'; 
-	RI = 0;
-	//JUDGE FANS STAGE
-	if (Received_Buf[4] == '0') FAN_FLAG = 0;
-	else if (Received_Buf[4] == '1') FAN_FLAG = 1;
-	else if (Received_Buf[4] == '2') FAN_FLAG = 2;
-	else if (Received_Buf[4] == '3') FAN_FLAG = 3;
-	else FAN_FLAG = 0;
-	//JUDGE TIME
-	if (Received_Buf[0] >= '6') TIME_FLAG = 0;
-	else TIME_FLAG = 1;
-	if (TIME_FLAG) {
-	  TIME_MIN = (Received_Buf[0] - 0x30) * 10 + (Received_Buf[1]-0x30);
-	  TIME_SEC = (Received_Buf[2] - 0x30) * 10 + (Received_Buf[3]-0x30);
-	} 
-	UART_send_string(Received_Buf);
-	for (i=19;i>0;i--)
-	  Received_Buf[i] = 0;
+	else { RI = 0; }  	
   }
   //如果发送完毕，清除标志位
   if(TI == 1) {	TI = 0; }
@@ -69,13 +72,14 @@ void UART() interrupt 4 {
 
 void test() {
   ES = 0;
-  Send_Buf[0] = (TIME_MIN / 10) + 0x30;
-  Send_Buf[1] = (TIME_MIN % 10) + 0x30;
-  Send_Buf[2] = (TIME_SEC / 10) + 0x30;
-  Send_Buf[3] = (TIME_SEC % 10) + 0x30;
-  Send_Buf[4] = FAN_FLAG + 0x30;
-  Send_Buf[5] = '!';
-  Send_Buf[6] = '\0';
+  Send_Buf[0] = '[';
+  Send_Buf[1] = (TIME_MIN / 10) + 0x30;
+  Send_Buf[2] = (TIME_MIN % 10) + 0x30;
+  Send_Buf[3] = (TIME_SEC / 10) + 0x30;
+  Send_Buf[4] = (TIME_SEC % 10) + 0x30;
+  Send_Buf[5] = FAN_FLAG + 0x30;
+  Send_Buf[6] = ']';
+  Send_Buf[7] = '\0';
   UART_send_string(Send_Buf);
   ES = 1;
 }
